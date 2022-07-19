886dd603fb0d85ac364ec8bfcd5eb11245be36db
oyk <oyk@oykui-MacBookPro.local>
2022-07-19 21:33:26 +0900

    update provider

--- practice/infra/providers.tf
+++ practice/infra/providers.tf
@@ -4,6 +4,22 @@ provider "aws" {
 
 terraform {
   backend "remote" {
+    hostname     = "app.terraform.io"
+    organization = "yk-study"
 
+    workspaces {
+      name = "terraform-study"
+    }
+  }
+  }
+
+terraform {
+  required_version = ">= 1.1.2"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.45.0"
+    }
   }
 }
