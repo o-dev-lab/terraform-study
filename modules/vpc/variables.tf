92ffee9a9fe78726df565800ac50c0fa6597a448
oyk <oyk@oykui-MacBookPro.local>
2022-07-19 21:23:40 +0900

    vpc

---
+++ modules/vpc/variables.tf
@@ -0,0 +1,7 @@
+variable "vpc_cidr" {}
+
+variable "tags" {}
+
+variable "env" {}
+
+variable "vpc_name" {}
---
+++ practice/infra/vpc.tf
@@ -0,0 +1,31 @@
+ ### VPC
+
+
+ locals {
+
+ }
+
+
+
+ variable "vpc_cidr" {
+   description = "VPC CIDR"
+   type        = string
+   default     = "192.168.0.0/16"
+ }
+ variable "vpc_name" {
+   description = "VPC Name"
+   type        = string
+   default     = "yk-vpc"
+ }
+ variable "env" {
+   description = "Project Environment"
+   type        = string
+   default     = "prd"
+ }
+
+module "vpc" {
+  source = "../../modules/vpc"
+  vpc_cidr = var.vpc_cidr
+  vpc_name = var.vpc_name
+  env = var.env
+}
