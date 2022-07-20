d142cadd4dc687dbd138eaa853a244c50f680524
oyk <mydhdbrud@gmail.com>
2022-07-20 21:20:29 +0900

    Route

--- practice/infra/vpc.tf
+++ practice/infra/vpc.tf
@@ -56,10 +56,11 @@ module "nat" {
 ############################################################################################################
 
 module "rt" {
-  count = length(local.subnet_cidrs)
+#  count = length(local.subnet_cidrs)
   source = "../../modules/network/route"
+  for_each = local.subnet_cidrs
 
-  env    = local.subnet_cidrs[count.index]
+  env    = each.key
 #  routings = {}
 #  rt_id = ""
 #  subnetting = each.value
